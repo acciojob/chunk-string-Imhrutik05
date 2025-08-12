867152812485daf54a0c7a1b7cbf990ad45b6b05
Hrutik Chaudhari <hrutikchaudhari05@gmail.com>
2025-08-12 16:37:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function stringChop(str, size) {
   // your code here
+	let result = [];
+	let curr = '';
+	for (let i = 0; i < str.length; i++){
+		let char = str[i];
+		if (i === str.length % size){
+			result.push(curr);
+		}
+		if (curr.length < size){
+			curr += char;
+		} else {
+			result.push(curr);
+			curr = char;
+		}
+	}	
 }
 
 // Do not change the code below
-const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size.");
-alert(stringChop(str, size));
+
